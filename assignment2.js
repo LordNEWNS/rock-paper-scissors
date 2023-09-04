e818328d5bc6ca6364d836820ba47480a64af0bf
LordNEWNS <80376074+LordNEWNS@users.noreply.github.com>
2023-09-04 05:19:18 -0400

    Add is game over function
    Add a function to check to see if the game is over.

--- assignment2.js
+++ assignment2.js
@@ -7,8 +7,12 @@ let playerChoice;
 let playerWins = 0;
 let playerLosses = 0;
 let draws = 0;
-const dellayInMs = 200 
-const displaMessage = document.querySelector('#message')
+const dellayInMs = 200 ;
+const displaMessage = document.querySelector('#message');
+let numberOfRounds = 0;
+const winColor = 'greenyellow'
+const lossColor = 'rgb(109, 30, 6)'
+const tieColor = 'grey'
 
 playerSelection.forEach(btn => {
     btn.addEventListener('click', (e) => {
@@ -17,6 +21,34 @@ playerSelection.forEach(btn => {
         playRound();
     });
 });
+
+function isGameOver(numberOfRounds) {
+    const bod = document.getElementById('bod')
+    const title = document.getElementById('title')
+    if (numberOfRounds === playerWins || numberOfRounds === playerLosses) {
+        while (bod.firstChild) {
+            bod.removeChild(bod.firstChild);
+        };
+        const button = document.createElement('button');
+        button.textContent = 'NEW GAME';
+        button.setAttribute('id', 'NEWGAME');
+        bod.appendChild(button);
+
+        title.textContent = getGameOverMessage();
+        if (playerWins > playerLosses) {
+            title,color = winColor
+        } else title.color = lossColor
+
+    }
+};
+
+function getGameOverMessage() {
+    if (playerWins > playerLosses) {
+        return('Congrats! you won ' + playerWins + ' to ' + playerLosses);
+    } else {
+        return('OOF you lost ' + playerLosses + ' to ' + playerWins)
+    }
+};
 
 function getWinMessage() {
     return('You win! ' + playerChoice + ' beats ' + compChoice);
@@ -38,54 +70,49 @@ function getCompChoice() {
 
 function playRound() {
     getCompChoice(); 
+    let e = document.getElementById('showMessage');
+    e.textContent = '';
+    let e2 = document.getElementById('showMessage2');
     if (playerChoice === compChoice) {
         ++draws;
         
-        // change game status message
-        let e = document.getElementById('showMessage');
-        e.textContent = '';
         setTimeout(() => {e.textContent = 'TIE!'}, dellayInMs);
-        e.style.color = 'grey';
+        e.style.color = tieColor;
 
         // change tie score
         let t = document.getElementById('tie');
         setTimeout(() => {t.textContent = draws}, dellayInMs);
-        t.style.color = 'grey';
+        t.style.color = tieColor;
 
         // change bottom message
-        let e2 = document.getElementById('showMessage2');
         setTimeout(() => {e2.textContent = getDrawMessage()}, dellayInMs);
-        e2.style.color = 'grey';
+        e2.style.color = tieColor;
     } else if 
     ((playerChoice === rock && compChoice == scissors)
     || (playerChoice === scissors && compChoice == paper) 
     || (playerChoice === paper && compChoice === rock)) {
         ++playerWins;
-        let e = document.getElementById('showMessage');
-        e.textContent = '';
+        
         setTimeout(() => {e.textContent = 'WIN!'}, dellayInMs);
-        e.style.color = 'greenyellow';
+        e.style.color = winColor;
 
         let t = document.getElementById('win');
         setTimeout(() => {t.textContent = playerWins}, dellayInMs);
-        t.style.color = 'greenyellow';
+        t.style.color = winColor;
 
-        let e2 = document.getElementById('showMessage2');
         setTimeout(() => {e2.textContent = getWinMessage()}, dellayInMs);
-        e2.style.color = 'greenyellow';
+        e2.style.color = winColor;
     } else {
         ++playerLosses;
-        let e = document.getElementById('showMessage');
-        e.textContent = '';
+        
         setTimeout(() => {e.textContent = 'LOSS'}, dellayInMs); 
-        e.style.color = 'rgb(109, 30, 6)';
+        e.style.color = lossColor;
 
         let t = document.getElementById('lose');
         setTimeout(() => {t.textContent = playerLosses}, dellayInMs);
-        t.style.color = 'rgb(109, 30, 6)';
+        t.style.color = lossColor;
 
-        let e2 = document.getElementById('showMessage2');
         setTimeout(() => {e2.textContent = getLossMessage()}, dellayInMs);
-        e2.style.color = 'rgb(109, 30, 6)';
-    }
+        e2.style.color = lossColor;
+    };
 };
